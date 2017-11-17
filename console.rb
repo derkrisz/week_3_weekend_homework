22cceb3a54ee60a1ef6b5c7c8b5a431af5b30e5c
derkrisz <krisztian.dergmail.com>
2017-11-17 14:19:37 +0000

    adds CRUD to tickets

--- console.rb
+++ console.rb
@@ -24,5 +24,14 @@ film2.save
 film3.save
 film4.save
 
+ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
+ticket2 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
+ticket3 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film3.id})
+ticket4 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film4.id})
+ticket1.save
+ticket2.save
+ticket3.save
+ticket4.save
+
 binding.pry
 nil
--- models/ticket.rb
+++ models/ticket.rb
@@ -9,4 +9,41 @@ def initialize(options)
   @film_id = options['film_id'].to_i
 end
 
+def save
+  sql = "INSERT INTO tickets (customer_id, film_id) VALUES ($1, $2) RETURNING *"
+  values = [@customer_id, @film_id]
+  result = SqlRunner.run(sql, values).first
+  @id = result["id"].to_i
+end
+
+def self.select_all
+  sql = "SELECT * FROM tickets"
+  tickets = SqlRunner.run(sql)
+  result = tickets.map { |ticket| Ticket.new(ticket)}
+  return result
+end
+
+def self.delete_all
+  sql = "DELETE FROM tickets"
+  SqlRunner.run(sql)
+end
+
+def delete
+  sql = "DELETE from tickets WHERE id = $1"
+  values = [@id]
+  SqlRunner.run(sql, values)
+end
+
+def self.list
+  sql = "SELECT * FROM tickets"
+  tickets = SqlRunner.run(sql)
+  return tickets.map { |ticket| Ticket.new(ticket)}
+end
+
+def update
+  sql = "UPDATE tickets SET $1 $2 WHERE id = $3"
+  values = [@customer_id, @film_id, @id]
+  SqlRunner.run(sql, values)
+end
+
 end
